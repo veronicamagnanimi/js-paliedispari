d2d2ccdb143077f25eb9f128ec8d143a373da9b1
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-05 16:39:50 +0100

    debug: funzione

--- PARIDISPARI/JAVASCRIPT/function.js
+++ PARIDISPARI/JAVASCRIPT/function.js
@@ -1,12 +1,9 @@
 //GENERO IL NUMERO RANDOM DA 1 A 5 PER IL COMPUTER USANDO LA FUNZIONE
+let result = randomNum (1, 5);
 function randomNum(min, max) {
   const randomInt = Math.floor(Math.random() * (max - min + 1) + min);
   return randomInt;
 }
-
-// //Test
-let result = randomNum (1, 5);
-console.log(`Il numero del computer è ${result}`);
 
 
 //STABILISCO SE LA SOMMA E' PARI O DISPARI
@@ -20,6 +17,7 @@ function sumEvenOdd(sum) {
     }
     return final;
 }
+console.log(`La somma dei due numeri è ${sumEvenOdd()}`);
 
 
 
--- PARIDISPARI/JAVASCRIPT/script.js
+++ PARIDISPARI/JAVASCRIPT/script.js
@@ -5,14 +5,16 @@ const number = parseInt(prompt("Scegli un numero da 1 a 5"));  // --> da stringa
 console.log(`L'utente ha scelto di essere ${evenOdd}`);
 console.log(`Il numero scelto è ${number}`);
 
+//Numero random computer
+console.log(`Il numero del computer è ${result}`);
+
 //Dato il numero random function, lo sommo al numero scelto dall'utente
 //ESECUZIONE LOGICA
-// let randomInt;
-
 let sum = number + result;
 console.log(`La somma dei due numeri è ${sum}`);
 
-console.log(`La somma dei due numeri è ${sumEvenOdd()}`);
+//Verifico se la somma del numero random e del numero scelto dall'utente è pari o dispari (funzione)
+console.log(`La somma dei due numeri è ${sumEvenOdd(sum)}`);
 
 //OUTPUT VINCITORE
 let vincitore;
